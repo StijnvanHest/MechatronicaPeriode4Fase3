463123ff732c2964600665c6adf3ed2795384da1
Stijn <stijnvanhest@gmail.com>
2020-06-05 09:41:20 +0200

    camera state verbeterd

--- ariac_behaviors/ariac_flexbe_states/src/ariac_flexbe_states/detect_first_part_camera_ariac_state.py
+++ ariac_behaviors/ariac_flexbe_states/src/ariac_flexbe_states/detect_first_part_camera_ariac_state.py
@@ -68,7 +68,7 @@ class DetectFirstPartCameraAriacState(EventState):
 
 	<= continue 				if the pose of the part has been succesfully obtained
 	<= failed 				otherwise
-
+	<= not_found				Camera did not find your given parts
 	'''
 
 	def __init__(self, part_list, time_out = 0.5):
@@ -117,7 +117,7 @@ class DetectFirstPartCameraAriacState(EventState):
 						return 'continue'
 			userdata.pose = None
 			userdata.part = None
-			return 'failed'
+			return 'not_found'
 
 	def on_enter(self, userdata):
 		# This method is called when the state becomes active, i.e. a transition from another state to this one is taken.
